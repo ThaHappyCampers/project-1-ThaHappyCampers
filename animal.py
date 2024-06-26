ebc7f61eb9582b2590aef12f359c559486bf6c83
ThaHappyCampers <garcij32@oregonstate.edu>
2024-06-25 23:49:29 -0700

    j

---
+++ animal.py
@@ -0,0 +1,10 @@
+# project-1
+
+# Author: Joshua Garcia
+# GitHub username: ThaHappyCampers
+# Date: 6/25/2024
+# Description: Asks the user for their favorite animal and then
+#              prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
