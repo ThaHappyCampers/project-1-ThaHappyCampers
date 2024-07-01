5edcf38581e8ad3dcb94d5f8c448b8b27d5327cd
ThaHappyCampers <garcij32@oregonstate.edu>
2024-07-01 09:26:28 -0700

    Assignment 2a

---
+++ temp_convert.py
@@ -0,0 +1,13 @@
+# project-2b
+
+# Author: Joshua Garcia
+# GitHub username: ThaHappyCampers
+# Date: 7/1/2024
+# Description: Write a program that converts Celsius temperatures to Fahrenheit temperatures.
+
+print("Please enter a Celsius temperature.")
+# Celsius(c) to Fahrenheit(f): ((c*9/5)+32)=f
+num_1 = float(input())
+temp = ((num_1 * 9/5) + 32)
+print("The equivalent Fahrenheit temperature is:")
+print(temp)
